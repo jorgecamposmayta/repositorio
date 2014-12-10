9acddb463b2c92e0301a6fd344b1c2b5cc57e39f
casa <casa@192.168.1.34>
2014-12-10 16:48:20 -0500

    comitt de casa

---
+++ src/paquete/clasesita.java
@@ -0,0 +1,13 @@
+package paquete;
+
+public class clasesita {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("paquete actualizado");
+	}
+
+}
