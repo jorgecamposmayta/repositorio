cb74adc408efe0f29a1026569342f1fe93210f21
casa <casa@192.168.1.34>
2014-12-10 17:09:31 -0500

    casa 2

--- src/paquete/clasesita.java
+++ src/paquete/clasesita.java
@@ -7,7 +7,7 @@ public class clasesita {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("paquete actualizado desde casa ");
+			System.out.println("paquete actualizado desde casa");
 	}
 
 }
