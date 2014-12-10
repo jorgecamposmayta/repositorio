c48d80f7fca36ca3cc397c3f1c5b899063374711
casa <casa@192.168.1.34>
2014-12-10 17:18:57 -0500

    branch 4

--- src/paquete/clasesita.java
+++ src/paquete/clasesita.java
@@ -7,7 +7,7 @@ public class clasesita {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("paquete  actualizado desde casa esto es un brancha");
+			System.out.println("paquete  actualizado  desde casa esto es un brancha ahora si este  4");
 	}
 
 }
